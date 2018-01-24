2f83ee409e2775b6faf9707246b3df2d006af2ba
Milan Timko <milan.timko@akademiasovy.sk>
2018-01-24 11:56:27 +0100

    Weather

---
+++ weatherjs.js
@@ -0,0 +1,17 @@
+$("#document").ready(function(){
+        $("#submit").click(function(){
+        	var city = $("#city").val();
+        	var country = $("#country").val();
+        		if (city.length>1) {
+        			var urllink='http://api.openweathermap.org/data/2.5/weather?q=';
+        			urllink = urllink + city ; 
+        			if (country.length == 2) {
+        				urllink = urllink + ',' + country;
+        				urllink = urllink + '&appid=a61d75f85751f6111397e5a96d6484d7';
+        				console.log(urllink);
+        			}
+        		}
+        
+    });
+
+});
