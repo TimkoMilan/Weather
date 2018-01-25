1adf23bc6133c418019f9584bad4f95031548721
TimkoMilan <32731715+TimkoMilan@users.noreply.github.com>
2018-01-25 10:12:28 +0100

    Table

--- weatherjs.js
+++ weatherjs.js
@@ -1,66 +1,89 @@
-$(function(){
+$(document).ready(function(){
+    $("#submit").click(function(){
+        var city=$("#city").val();
+        var code=$("#country").val();
+        if (city.length>1) {
+            var urllink='https://api.openweathermap.org/data/2.5/weather?q=';
+            urllink=urllink + city;
+            if (code.length==2){
+                urllink=urllink+','+code;
+            }
+            urllink=urllink+'&APPID=a61d75f85751f6111397e5a96d6484d7';
 
-$("#submit").click(function(){
-    var city = $("#city").val();
-    var country = $("#country").val();
+            $.ajax(
+            {
+                url: urllink,
+                data: {format: 'json'},
+                error: function()
+                {
+                    //chyba
+                },
+                dataType: 'json',
+                success: function(data)
+                {
+                    console.log("temp: "+data.main.temp);
+                    console.log("desc: "+data.weather[0].description);
 
-    if(city.length > 1){
-        var urlLink = 'http://api.openweathermap.org/data/2.5/weather?q=';
-        urlLink = urlLink + city;
+                     $('#main').empty();
+                var table=$('<table/>');
+                var tr=getTr('City:', city);
+                table.append(tr);
+                $('#form').append(table);
+                
+                var tr=getTr('Country:', $('#entercity').val());
+                table.append(tr);
+                
+                var tr=getTr('Temperature:', parseFloat(data.main.temp-273.15).toFixed(1)+" ℃");
+                table.append(tr);
+                
+                var tr=getTr('Humidity:', data.main.humidity+" %");
+                table.append(tr);
+                
+                var tr=getTr('Description:', data.weather[0].description);
+                table.append(tr);
+                
+                var tr=getTr('Pressure:', data.main.pressure+' hPa');
+                table.append(tr);
+                
+                if($("#details").is(":checked") == true){
+                    
+                    
+                    var tr=getTr('Sunrise:', data.sys.sunrise);
+                    table.append(tr);
+                    
+                    var tr=getTr('Sunset:', data.sys.sunset);
+                    table.append(tr);
+                    
+                    var tr=getTr('Wind:', data.wind.speed+' m/s');
+                    table.append(tr); 
+                    
+                    var tr=getTr('Min temperature:', parseFloat(data.main.temp_min-273.15).toFixed(1)+" ℃");
+                    table.append(tr);
+                    
+                    var tr=getTr('Max temperature:', parseFloat(data.main.temp_max-273.15).toFixed(1)+" ℃");
+                    table.append(tr);
+                    
+                    var tr=getTr('Min temperature:', data.visibility);
+                    table.append(tr);
+                };
+
+
+                },
+                type: 'GET'
+            });
+        }
+    });
+    function getTr(data1,data2){
+    
+        var tr=$('<tr/>');
+        var td1=$('<td/>');
+        $(td1).append(data1);
+        var td2=$('<td/>');
+        $(td2).append(data2);
+        tr.append(td1);
+        tr.append(td2); 
+                    
+                    return tr;
+
     }
-
-    if(country.length == 2){
-        urlLink = urlLink + ',' + country;
-        
-    }
-
-    urlLink = urlLink + '&APPID=a61d75f85751f6111397e5a96d6484d7 ';
-    console.log(urlLink);
-
-        $.ajax({
-            url:urlLink,
-            data: { format: 'json'},
-            error: function(){
-                console.log('error');
-            },
-            dataType: "json",
-            success : createTable,
-            type: 'GET'
-
-        });
-
-});
-
-function createTable(data){
-
-    $("#main").empty();
-
-    var table = $('<table/>');
-    table.addClass('table table-bordered');
-    $("#main").append(table);
-
-    table.append(getTableRow('Temperature: ', Math.round(data.main.temp-273.15),'Humidity: ',data.main.humidity+'%' ));
-    table.append(getTableRow('Description: ', data.weather[0].description,'Pressure: ',data.main.pressure + ' hPa' ));
-
-    if($('#showDetails').is(":checked")){
-
-        table.append(getTableRow('Sunrise: ', data.sys.sunrise,'Sunset: ',data.sys.sunset ));
-        table.append(getTableRow('Wind speed: ', data.wind.speed + 'm/s','Visibility: ',data.visibility ));
-        table.append(getTableRow('Min temperature: ', Math.round(data.main.temp_min-273.15),'Max temperature: ',Math.round(data.main.temp_max-273.15) ));
-
-    }
-
-}
-
-function getTableRow(name1,data1,name2,data2){
-    var row = $('<tr/>');
-    var column = $('<td/>');
-    var column2 = $('<td/>');
-    row.append(column);
-    column.html(name1 + data1);
-    row.append(column2);
-    column2.html(name2 + data2);
-    return row;
-}
-
 });
